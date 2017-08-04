6bc4958fbef5937c16950808f8aa0df70ae55f86
ecramirez94 <ecramirez94@gmail.com>
2017-08-04 16:39:39 -0400

    Added blink functions

--- carlos_folder/blink_test/blink_test.ino
+++ carlos_folder/blink_test/blink_test.ino
@@ -1,8 +1,16 @@
+int one = 0;
+int two = 1;
+int three = 2;
 int left = 5;
 int right = 6;
 
+int nite[5] = {one, two, three, left, right};
+
 void setup() {
-  
+
+  pinMode(one, OUTPUT);
+  pinMode(two, OUTPUT);
+  pinMode(three, OUTPUT);
   pinMode(left, OUTPUT);
   pinMode(right, OUTPUT);
 
@@ -10,8 +18,8 @@ void setup() {
 
 void loop() {
 
-  ledAlert("offline");
-  delay(2000);
+  ledAlert("sweep");
+//  delay(1000);
   
 }
 
@@ -39,6 +47,43 @@ void ledAlert(String mode)
     digitalWrite(left, LOW);
     digitalWrite(right, LOW);
     delay(100);
+    }
+  }
+  else if (mode == "relay2")
+  {
+    for(int i = 0; i <= 8; i++)
+    {
+      digitalWrite(two, HIGH);
+      delay(25);
+      digitalWrite(two, LOW);
+      digitalWrite(left, HIGH);
+      delay(45);
+      digitalWrite(left, LOW);
+      digitalWrite(one, HIGH);
+      delay(25);
+      digitalWrite(one, LOW);
+      digitalWrite(right, HIGH);
+      delay(45);
+      digitalWrite(right, LOW);
+      digitalWrite(three, HIGH);
+      delay(25);
+      digitalWrite(three, LOW);
+    }
+  }
+  else if (mode == "sweep")
+  {
+    for (int i = 0; i <= 4; i++)
+    {
+      digitalWrite(nite[i], HIGH);
+      delay(20);
+      digitalWrite(nite[i], LOW);
+    }
+    delay(10);
+    for (int i = 4; i >= 0; i--)
+    {
+      digitalWrite(nite[i], HIGH);
+      delay(20);
+      digitalWrite(nite[i], LOW);
     }
   }
   else if (mode == "offline")
