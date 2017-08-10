3fe7f4ed44054a69fe0ec8bb4f41b2a74136b750
591622 <kylefoster@c02t5150hf1t.users.bah.com>
2017-08-10 13:47:56 -0400

    change recieve message blink length

--- master_code/Final_Demo_Scripts/node5_presentation/node5_presentation.ino
+++ master_code/Final_Demo_Scripts/node5_presentation/node5_presentation.ino
@@ -594,7 +594,7 @@ void ledAlert(String mode)
     digitalWrite(red, HIGH);
     digitalWrite(white, HIGH);
     digitalWrite(blue, HIGH); 
-    delay(2000);
+    delay(4000);
     digitalWrite(left, LOW);
     digitalWrite(right, LOW);
     digitalWrite(red, LOW);
